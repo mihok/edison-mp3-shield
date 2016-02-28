437b8de894a6c1bf6080257c20ea3d1b857e1a9d
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-02-28 12:50:58 -0500

    while(1)

--- start.js
+++ start.js
@@ -2,6 +2,12 @@
 var MP3Shield = require('./libs/MP3PlayerShield');
 
 var wave = new MP3Shield();
+var EXIT = 0;
+
 wave.setup(function () {
   console.log('MP3Shield:', 'Ready');
+
+  EXIT = 1;
 });
+
+while(!EXIT);
