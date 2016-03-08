796e56742767cf424c7546ca5ba678392d8dba53
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-03-08 06:27:55 -0500

    trying old script

--- start.js
+++ start.js
@@ -1,5 +1,5 @@
 // var Edison = require('./libs/EdisonArduinoBreakout')
-var MP3Shield = require('./libs/MP3PlayerShield2');
+var MP3Shield = require('./libs/MP3PlayerShield');
 
 var wave = new MP3Shield();
 var loop = function () {
