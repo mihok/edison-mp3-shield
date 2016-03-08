b50bcdacfbd97481b1103935e06d6d7e0001c84d
Matthew Mihok <4D.69.68.6F.6B@gmail.com>
2016-03-08 06:20:25 -0500

    make loop

--- start.js
+++ start.js
@@ -2,9 +2,13 @@
 var MP3Shield = require('./libs/MP3PlayerShield2');
 
 var wave = new MP3Shield();
-wave.setup();
-// var EXIT = 0;
+var loop = function () {
+  console.log('PING PONG');
+  setTimeout(loop, 1000);
+};
 
+wave.setup();
+loop();
 // var EXIT_timeout = function () {
 //   EXIT = 1;
 // };
